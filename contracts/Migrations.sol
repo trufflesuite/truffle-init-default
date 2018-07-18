3768879b7bc9c65a866087373d5071321c4f6bcf
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-07-18 14:28:52 +0200

    Update Migrations.sol to latest Solidity syntax

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,22 +1,22 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.24;
 
 contract Migrations {
   address public owner;
-  uint public last_completed_migration;
+  uint256 public last_completed_migration;
 
   modifier restricted() {
     if (msg.sender == owner) _;
   }
 
-  function Migrations() {
+  constructor() public {
     owner = msg.sender;
   }
 
-  function setCompleted(uint completed) restricted {
+  function setCompleted(uint256 completed) public restricted {
     last_completed_migration = completed;
   }
 
-  function upgrade(address new_address) restricted {
+  function upgrade(address new_address) public restricted {
     Migrations upgraded = Migrations(new_address);
     upgraded.setCompleted(last_completed_migration);
   }
