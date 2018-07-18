202a01a5dca1c185b06e0addd7ac73180626a6e8
g. nicholas d'andrea <gnidan@users.noreply.github.com>
2018-07-18 14:41:14 +0200

    Fix visibility modifier

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -24,7 +24,7 @@ contract MetaCoin {
 		return true;
 	}
 
-	function getBalanceInEth(address addr) public pure returns(uint){
+	function getBalanceInEth(address addr) public view returns(uint){
 		return ConvertLib.convert(getBalance(addr),2);
 	}
 
