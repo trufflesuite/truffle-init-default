a1afcc585354e991b6490d8ba93273b112210a37
eggplantzzz <tmfeickert@gmail.com>
2020-09-25 15:38:21 -0400

    Comment out develop network in truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 5000000
-    }
+    // development: {
+    //  host: "localhost",
+    //  port: 8545,
+    //  network_id: "*", // Match any network id
+    //  gas: 5000000
+    // }
   },
   compilers: {
     solc: {
